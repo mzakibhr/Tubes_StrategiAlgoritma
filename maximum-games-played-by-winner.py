55698805ee071320c2de5c826bffa35a11c51d9d
kazakizi <75782161+mzakibhr@users.noreply.github.com>
2021-06-10 21:34:21 +0700

    Update maximum-games-played-by-winner.py

--- maximum-games-played-by-winner.py
+++ maximum-games-played-by-winner.py
@@ -1,8 +1,8 @@
 import math
 import timeit
-def maxGameByWinnerDynamic(N):
+def maxGameByWinnerDynamic(n):
     start = timeit.default_timer()
-    dp = [0 for jumlahpermainan in range(N)]
+    dp = [0 for jumlahpermainan in range(n)]
      
     # jika hanya 1 pemain maka hanya terdapat 0 permainan
     # untuk 1 permainan maka dibutuhkan 2 pemain
@@ -10,38 +10,38 @@ def maxGameByWinnerDynamic(N):
     dp[1] = 2
      
     # loop sampai ke-jumlahpermainan Fibonacci
-    # bilangan kurang dari atau sama dengan N
+    # bilangan kurang dari atau sama dengan n
     jumlahpermainan = 1
-    while dp[jumlahpermainan] <= N:
+    while dp[jumlahpermainan] <= n:
         jumlahpermainan = jumlahpermainan + 1
         dp[jumlahpermainan] = dp[jumlahpermainan - 1] + dp[jumlahpermainan - 2]
 
     # hasil (jumlahpermainan - 1) ditambahkan 1 pada pembulatan pada loop while   
-    # dan hasil yang diinginkan adalah nilai terakhir, yaitu yang lebih kecil dari N
+    # dan hasil yang diinginkan adalah nilai terakhir, yaitu yang lebih kecil dari n
     stop = timeit.default_timer()
     execution_time = stop - start
     return (jumlahpermainan - 1), execution_time
 
-def maxGameByWinnerBruteForce(N):
+def maxGameByWinnerBruteForce(n):
     start = timeit.default_timer()
     jumlahpermainan = 0 #jumlah permainan dimulai dari 0 permainan
     # akan dilakukan loop sampai pemain kurang dari 2 pemain
     i = 2
     while i <= n:
-        N = N/2
+        n = n/2
         jumlahpermainan = jumlahpermainan + 1
-        if N % 2 != 0: # jika ganjil maka pemain akan dibulatkan untuk permainan selanjutnya
-            N = math.ceil(N)
+        if n % 2 != 0: # jika ganjil maka pemain akan dibulatkan untuk permainan selanjutnya
+            n = math.ceil(n)
     stop = timeit.default_timer()
     execution_time = stop - start
     return jumlahpermainan, execution_time
  
 # Main Program
-N = int(input("Masukkan Jumlah Pemain: "))
+n = int(input("Masukkan Jumlah Pemain: "))
 print("=======================================")
-print("Berdasarkan Algoritma Brute Force\nJumlah Pemain :", N,"\nUntuk Menang Membutuhkan", maxGameByWinnerBruteForce(N)[0], "Permainan")
-print("Waktu Eksekusi: ",maxGameByWinnerBruteForce(N)[1], "seconds")
+print("Berdasarkan Algoritma Brute Force\nJumlah Pemain :", n,"\nUntuk Menang Membutuhkan", maxGameByWinnerBruteForce(n)[0], "Permainan")
+print("Waktu Eksekusi: ",maxGameByWinnerBruteForce(n)[1], "seconds")
 print("=======================================")
 print("=======================================")
-print("Berdasarkan Algoritma Dynamic\nJumlah Pemain :", N, "\nUntuk Menang Membutuhkan", maxGameByWinnerDynamic(N)[0], "Permainan")
-print("Waktu Eksekusi: ",maxGameByWinnerDynamic(N)[1], "seconds")
+print("Berdasarkan Algoritma Dynamic\nJumlah Pemain :", n, "\nUntuk Menang Membutuhkan", maxGameByWinnerDynamic(n)[0], "Permainan")
+print("Waktu Eksekusi: ",maxGameByWinnerDynamic(n)[1], "seconds")
